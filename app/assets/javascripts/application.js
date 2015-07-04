206cc0c6bbaa93e025273b30eb940aabcfa16305
Ben Whittle <benwhittle31@gmail.com>
2015-07-04 14:17:25 -0700

    Fuck BBone's pushState requirement

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,10 @@ define(["backbone", "router"], function(Backbone, Router) {
     var App = {};
     App.start = function() {
         new Router();
-        Backbone.history.start();
+        Backbone.history.start({
+        	// add this so that you can actually do "www.whatever.com/extension" rather than "www.whatever.com/#extension"
+        	pushState: true
+        });
     };
     App.start();
     return App;
